0ed5ed01736191615251e7ed58fba0f21f4f2630
Ashmit Pai <ashmitpai2009@gmail.com>
2025-11-24 21:57:35 -0800

    wordle.py part 1 done

--- wordle.py
+++ wordle.py
@@ -25,25 +25,13 @@ class Wordle:
         Args:
             words_path: Path to the file containing the words.
         """
+
+
         with open(words_path, "r") as file:
             self.words: List[str] = [line.strip() for line in file.readlines()]
         self.reset()
     
-    def guess(self, word: str) -> List[int]:
-        """
-        Process a guess, and return the feedback.
-
-        Args:
-            word (str): The guessed word. Must be a valid word in the word list.
-
-        Returns:
-            A list of integers, where each integer represents the feedback of the guess.
-                0: The letter is not in the word.
-                1: The letter is in the word, but in the wrong position.
-                2: The letter is in the word, and in the correct position.
-        """
-        # TODO: Part 1.III.B
-        pass # Remove this line when you have implemented the method.
+    
 
     @staticmethod
     def get_feedback(guess_word: str, candidate_word: str) -> str:
@@ -66,9 +54,58 @@ class Wordle:
         it is in the wrong position. The other occurrences will be marked as 0 (not in the word).
         This mimics NYT Wordle's behavior of not overcounting duplicate letters in a guess.
         """
-        # TODO: Part 1.III.A
-        pass # Remove this line when you have implemented the method.   
-    
+        candidate_word_freq = {}
+        for letter in candidate_word:
+            if letter in candidate_word_freq:
+                candidate_word_freq[letter] += 1
+            else:
+                candidate_word_freq[letter] = 1            
+      
+        feedback = []
+        
+        for i in range(len(guess_word)):
+            if guess_word[i] == candidate_word[i]:
+                feedback.append(2)
+                candidate_word_freq[guess_word[i]] -= 1
+                if candidate_word_freq[guess_word[i]] == 0:
+                    del candidate_word_freq[guess_word[i]]
+            else:
+                feedback.append(0)
+
+        for i in range(len(guess_word)):
+            if guess_word[i] in candidate_word_freq and guess_word[i] != candidate_word[i]:
+                candidate_word_freq[guess_word[i]] -= 1
+                if candidate_word_freq[guess_word[i]] == 0:
+                    del candidate_word_freq[guess_word[i]]
+                feedback[i] = 1
+            
+        return feedback
+
+
+    def guess(self, word: str) -> List[int]:
+        """
+        Process a guess, and return the feedback.
+
+        Args:
+            word (str): The guessed word. Must be a valid word in the word list.
+
+        Returns:
+            A list of integers, where each integer represents the feedback of the guess.
+                0: The letter is not in the word.
+                1: The letter is in the word, but in the wrong position.
+                2: The letter is in the word, and in the correct position.
+        """
+        # TODO: Part 1.III.B
+
+        while word not in self.words:
+            word = input("Pick a valid word: ")
+
+        self.num_guesses -= 1
+        self.guesses.append(word)
+
+        return self.get_feedback(word, self.target_word)  
+
+
     def reset(self) -> str:
         """
         Reset the game state, by selecting a new random word, and resetting the guess counter.
@@ -76,8 +113,14 @@ class Wordle:
         Returns:
             The target word.
         """
-        # TODO: Part 1.I.B
-        pass # Remove this line when you have implemented the method.
+
+        self.target_word = random.choice(self.words)
+        self.reset_with_target_word(self.target_word)
+
+        
+
+
+
     
     def reset_with_target_word(self, word: str) -> None:
         """
@@ -86,8 +129,32 @@ class Wordle:
         Args:
             word (str): The target word.
         """
-        # TODO: Part 1.I.A
-        pass # Remove this line when you have implemented the method.
+
+        """
+         Attributes:
+        words (List[str]): The list of all words.
+        target_word (str): The target word to guess.
+        target_word_freqs (Dict[str, int]): The frequency of each letter in the target word.
+        guesses (List[str]): The list of guesses made by the player.
+        num_guesses (int): The number of guesses made by the player.
+        won (bool): Whether the player has won the game.
+    """
+        self.target_word = word
+        self.num_guesses = 6
+        self.won = False
+        
+        self.guesses = []
+        self.target_word_freqs = {}
+        for i in word:
+            if i in self.target_word_freqs:
+                self.target_word_freqs[i] += 1
+            else:
+                self.target_word_freqs[i] = 1
+
+
+
+
+        
     
     def game_over(self) -> bool:
         """
@@ -96,5 +163,10 @@ class Wordle:
         Returns:
             True if the game has ended, False otherwise.
         """
-        # TODO: Part 1.II
-        pass # Remove this line when you have implemented the method.
+        
+        if self.target_word in self.guesses:
+            return True
+        elif self.num_guesses == 0:
+            return True
+        else:
+            return False
